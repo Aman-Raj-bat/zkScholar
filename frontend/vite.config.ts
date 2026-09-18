17bb47b73b38986c877d46af1b32050fda519909
Aman Raj <amanraj@zkscholar.dev>
2026-09-18 14:35:52 +0530

    feat(frontend): add vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,3 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import topLevelAwait from 'vite-plugin-top-level-await';
-import wasm from 'vite-plugin-wasm';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  plugins: [react(), tailwindcss(), wasm(), topLevelAwait()],
-});
+export default defineConfig({ plugins: [react()], build: { target: 'es2022' } });
